a5bf56d44c9871f486f19c083a44bd51d8257286
Ankush Chhabra <ankushchhabra2002@gmail.com>
2024-12-30 23:44:19 +0530

    commit

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./DescriptionBox.css";
+const DescriptionBox = () => {
+  return (
+    <div className="descriptionbox">
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat
+          debitis perspiciatis odio minima ullam, hic, nisi nobis veniam
+          temporibus, ducimus cupiditate dolores iusto! Ex porro voluptas
+          dignissimos facilis suscipit blanditiis reprehenderit tempore aperiam
+          perferendis reiciendis autem illo unde, optio delectus aut voluptatum
+          velit? Illum a cum ab animi odit id rem deleniti repellat distinctio
+          officiis, possimus placeat eius voluptates. Deleniti illo quisquam
+          doloribus quibusdam maxime beatae necessitatibus voluptatum molestiae,
+          cupiditate perferendis tenetur officiis, omnis amet cum itaque eaque
+          quos explicabo.
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt in
+          aspernatur reiciendis harum accusamus, amet veritatis rem cumque
+          cupiditate sint iusto ea. Iusto, sint! Dicta sunt natus cumque labore
+          cum perspiciatis odit inventore dolore voluptatibus error, quasi porro
+          non praesentium?
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default DescriptionBox;
--- src/Components/ProductDisplay/ProductDisplay.jsx
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -1,8 +1,69 @@
 import React from "react";
 import "./ProductDisplay.css";
+import star_icon from "../Assets/star_icon.png";
+import star_dull_icon from "../Assets/star_dull_icon.png";
 
-const ProductDisplay = () => {
-  return <div className="productdisplay">ProductDisplay</div>;
+const ProductDisplay = (props) => {
+  const { product } = props;
+  return (
+    <div className="productdisplay">
+      <div className="productdisplay-left">
+        <div className="productdisplay-img-list">
+          <img src={product.image} alt="" />
+          <img src={product.image} alt="" />
+          <img src={product.image} alt="" />
+          <img src={product.image} alt="" />
+        </div>
+        <div className="productdisplay-img">
+          <img className="productdisplay-main-img" src={product.image} alt="" />
+        </div>
+      </div>
+      <div className="productdisplay-right">
+        <h1>{product.name}</h1>
+        <div className="productdisplay-right-start">
+          <img src={star_icon} alt="" />
+          <img src={star_icon} alt="" />
+          <img src={star_icon} alt="" />
+          <img src={star_icon} alt="" />
+          <img src={star_dull_icon} alt="" />
+          <p>(122)</p>
+        </div>
+        <div className="productdisplay-right-prices">
+          <div className="productdisplay-right-price-old">
+            ${product.old_price}
+          </div>
+          <div className="productdisplay-right-price-new">
+            ${product.new_price}
+          </div>
+        </div>
+        <div className="productdisplay-right-description">
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Porro
+          suscipit, commodi non exercitationem sed quae facere accusamus nam.
+          Sequi, expedita iste nesciunt consequatur quam fuga tenetur.
+          Laudantium fugit eligendi, obcaecati voluptate, veritatis non nobis
+          quae in ex debitis adipisci sapiente asperiores, consequatur atque
+          totam. Blanditiis pariatur eligendi molestiae quia laboriosam.
+        </div>
+        <div className="productdisplay-right-size">
+          <h1>Select Size</h1>
+          <div className="productdisplay-right-sizes">
+            <div>S</div>
+            <div>M</div>
+            <div>L</div>
+            <div>XL</div>
+            <div>XXL</div>
+          </div>
+        </div>
+        <button>ADD TO CART</button>
+        <p className="productdisplay-right-category">
+          <span>Category :</span>Women, T-Shirt, Crop Top
+        </p>
+        <p className="productdisplay-right-category">
+          <span>Tags :</span>Modern, Latest
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default ProductDisplay;
